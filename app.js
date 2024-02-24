4cbc3334fd65a95672c15daa02b02ada91bae773
Nika K <firstnamenika@gmail.com>
2024-02-24 16:39:14 -0500

    additional dynamic styling and functionality in phases section && nav bar structure

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ function changeButton(id) {
     // Get the clicked button by looking up its id
     let ourButton = document.getElementById(id);
     
-    // Get the current state from the data-clicked attribute
-    let clicked = ourButton.getAttribute('data-clicked') === 'true';
+    // Initialize a notClicked state
+    let notClicked = ourButton.getAttribute('data-clicked') === 'true';
 
-    if (!clicked) { // if the button was clicked
+    if (!notClicked) { // if the button was clicked
         ourButton.innerHTML = 'COMPLETED'; // update contents
         ourButton.setAttribute('data-clicked', 'true'); // update state
     } else {
@@ -26,15 +26,16 @@ for (let i = 0; i < buttons.length; i++) {
 
 // Define low container
 const lowContainer = document.getElementById('low-container');
-lowContainer.addEventListener('mouseenter', enterLowContainer);
-lowContainer.addEventListener('mouseleave', leaveLowContainer);
+lowContainer.addEventListener('mouseenter', enterLowContainer); // listen for mouse to enter the container
+lowContainer.addEventListener('mouseleave', leaveLowContainer); // listen for mouse to leave the container
 
 // enterLowContainer function
 function enterLowContainer() {
     const issuesDiv = document.querySelector('.issues-div');
     const risksDiv = document.querySelector('.risks-div');
 
-    issuesDiv.style.backgroundColor = '#fcecd05d';
+    // update styling
+    issuesDiv.style.backgroundColor = '#fcecd05d'; 
     risksDiv.style.backgroundColor = '#ffdbdb5d';
 }
 
@@ -43,6 +44,56 @@ function leaveLowContainer() {
     const issuesDiv = document.querySelector('.issues-div');
     const risksDiv = document.querySelector('.risks-div');
 
+    // reverse styling
     issuesDiv.style.backgroundColor = 'transparent';
     risksDiv.style.backgroundColor = 'transparent';
+}
+
+
+// add raciMoveRight using event delegation
+
+function raciMoveRight(id) {
+    let raci = document.getElementById(id);
+    raci.classList.add('raci-move-right');
+}
+
+document.getElementById('raci-div').addEventListener('mouseover', function(event) {
+    if (event.target.classList.contains('raci-description')) {
+        raciMoveRight(event.target.parentElement.id);
+    }
+})
+
+// in phase container, listen if a phase was clicked on
+// clicked phase sends id to a function
+// function shows phases information in the title area
+
+const phases = document.getElementsByClassName('phase');
+
+for (let i = 0; i < phases.length; i++) {[
+    phases[i].addEventListener('click', function(event) {
+        showOverlay(this.id)
+    })
+]}
+
+function showOverlay(id) {
+    const phase = document.getElementById(id);
+    const information = phase.querySelector('.information');
+    const topTitle = document.getElementById('top-title');
+    const insert = document.getElementById('insert');
+
+    topTitle.innerHTML = '';
+    insert.innerHTML = information.innerHTML;
+    insert.classList.add('overlay');
+
+}
+
+function removeOverlay() {
+    const overlay = document.querySelector('.overlay');
+    const topTitle = document.getElementById('top-title');
+    const insert = document.getElementById('insert');
+
+    insert.classList.remove('overlay');
+    insert.innerHTML = '';
+    topTitle.innerHTML = "4 Phases Of Product Management";
+
 }
